110140657914a145dc19c12f042ea17abde45250
Christoph Barbian <christoph.barbian@sap.com>
2020-04-19 20:32:24 +0200

    Make get_cidrs aware of additional vpc cidr ranges, and add optional range restriction for automatic block determination

--- functions/get_cidrs/lambda_function.py
+++ functions/get_cidrs/lambda_function.py
@@ -10,11 +10,11 @@ alnum = string.ascii_uppercase + string.ascii_lowercase + string.digits
 ec2_client = boto3.resource('ec2')
 
 
-def get_cidrs(size, qty, vpc_id):
+def get_cidrs(size, qty, vpc_id, range):
     # TODO: add locking mechanism to prevent collisions when run concurrently
     vpc = ec2_client.Vpc(vpc_id)
     allocated_cidrs = netaddr.IPSet([s.cidr_block for s in vpc.subnets.all()])
-    unused_cidrs = netaddr.IPSet([vpc.cidr_block]) ^ allocated_cidrs
+    unused_cidrs = (netaddr.IPSet(map(lambda x : x['CidrBlock'],vpc.cidr_block_association_set)) ^ allocated_cidrs) & netaddr.IPSet([range])
     available_cidrs = []
     for sl in [list(s.subnet(size)) for s in unused_cidrs.iter_cidrs()]:
         available_cidrs = available_cidrs + sl
@@ -36,7 +36,8 @@ def handler(event, context):
             response_data['CidrBlocks'] = get_cidrs(
                 int(event['ResourceProperties']['CidrSize']),
                 int(event['ResourceProperties']['Qty']),
-                event['ResourceProperties']['VpcId']
+                event['ResourceProperties']['VpcId'],
+                event['ResourceProperties']['CidrRange'] if 'CidrRange' in event['ResourceProperties'] else '0.0.0.0/0'
             )
         cfnresponse.send(event, context, response_code, response_data, phys_id)
     except Exception as e:
